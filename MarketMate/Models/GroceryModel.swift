8998a1267d4768b64d6c688a5945f714ad29ae2e
primalCam <alphaglobalmedia@gmail.com>
2024-07-19 06:53:36 -0500

    - Added my data model for grocery items and lists - Next: Finish integration with listed names functionality

---
+++ MarketMate/Models/GroceryModel.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryModel.swift
+//  MarketMate
+//
+//  Created by Cameryn Smith on 7/19/24.
+//
+
+import SwiftUI
+
+struct GroceryModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroceryModel()
+}
---
+++ MarketMate/listGroups/addListView.swift
@@ -0,0 +1,18 @@
+//
+//  addListView.swift
+//  MarketMate
+//
+//  Created by Cameryn Smith on 7/19/24.
+//
+
+import SwiftUI
+
+struct addListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addListView()
+}
